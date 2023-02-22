ce2f0336dde181303b954363abceeeb65790b263
Hal J Rushton <digbads@gmail.com>
2023-02-21 17:19:07 -0700

    Initial Commit

---
+++ Physics/PhysicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhysicsApp.swift
+//  Physics
+//
+//  Created by Hal Rushton on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct PhysicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
